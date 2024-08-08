d772370ea506b442af30ce736ac49353c0336158
kishor saha <kishorsaha650@gmail.com>
2024-08-09 00:57:15 +0600

    loading page

--- src/app/snippets/[id]/page.js
+++ src/app/snippets/[id]/page.js
@@ -2,6 +2,7 @@ import { db } from "@/app/db"
 import { notFound } from "next/navigation"
 
 export default async function snippets(props){
+await new Promise((r)=>setTimeout(r,3000))
 
 const snippet = await db.snippet.findFirst({
     where: {id:parseInt(props.params.id)}
